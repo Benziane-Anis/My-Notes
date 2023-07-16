b8e541267a7a918dd5b5e57a4b16713640b55296
Benziane-Anis <anisbenziane03@gmail.com>
2023-07-16 18:24:32 +0100

    add button of the add not

--- lib/items/custom_addnote.dart
+++ lib/items/custom_addnote.dart
@@ -8,13 +8,26 @@ class AddNoteButtomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: Column(
-        children: [
-          SizedBox(height: 32,),
-          MyTexteField(hint: 'Title',maxLInes: 1),
-          SizedBox(height: 10,),
-          MyTexteField(hint: "Content...",maxLInes: 5),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 32,),
+            MyTexteField(hint: 'Title',maxLInes: 1),
+            SizedBox(height: 10,),
+            MyTexteField(hint: "Content...",maxLInes: 5),
+            SizedBox(height: 100,),
+            MaterialButton(onPressed: () {
+
+            },
+            height: 50,
+              textColor: Colors.black,
+              child: Text('ADD',style: TextStyle(fontSize: 20),),
+              color: Colors.tealAccent,
+              minWidth: MediaQuery.of(context).size.width,
+              shape: OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
+            )
+          ],
+        ),
       ),
     );
   }
