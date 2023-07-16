e6ba0b4865fec0df65df86ce2c51b52a777532c8
Benziane-Anis <anisbenziane03@gmail.com>
2023-07-16 17:57:35 +0100

    add custom MyTExtfield

--- lib/items/custom_addnote.dart
+++ lib/items/custom_addnote.dart
@@ -6,10 +6,14 @@ class AddNoteButtomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        MyTexteField()
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        children: [
+          SizedBox(height: 32,),
+          MyTexteField(),
+        ],
+      ),
     );
   }
 }
--- lib/items/custom_textfield.dart
+++ lib/items/custom_textfield.dart
@@ -5,6 +5,21 @@ class MyTexteField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextField();
+    return TextField(
+      style: TextStyle(fontSize: 22),
+      cursorColor: Colors.tealAccent,
+      decoration: InputDecoration(
+        hintText: "Title",
+        hintStyle: TextStyle(color: Colors.tealAccent,fontSize: 22,),
+        focusedBorder:OutlineInputBorder(
+            borderRadius: BorderRadius.circular(24),
+            borderSide: BorderSide(color: Colors.tealAccent)
+        ),
+        enabledBorder:OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(color: Colors.white)
+        ),
+      ),
+    );
   }
 }
